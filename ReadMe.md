3cf3737006c37c69fef1a2190c7ad41b323eead7
Ingy döt Net <yamlstar-release@yaml.com>
2026-06-19 14:55:46 -0400

    Add 0.1.4

---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+homebrew-yamlstar
+===================
+
+[Homebrew](https://brew.sh/) for YAMLStar
+
+
+## Install YAMLStar with Homebrew
+
+Works on Linux Intel and macOS ARM.
+
+```
+$ brew install yaml/yamlstar/libyamlstar
+$ brew install yaml/yamlstar/libyamlstar@0.1.4
+```
---
+++ libyamlstar@0.1.4.rb
@@ -0,0 +1,40 @@
+class LibyamlstarAT014 < Formula
+  desc "YAMLStar shared library"
+  homepage "https://github.com/yaml/yamlstar"
+  version "0.1.4"
+  license "MIT"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/yaml/yamlstar/releases/download/" \
+        "#{version}/libyamlstar-#{version}-linux-x64.tar.xz"
+      sha256 "2602f6da0d0ce92e90cc95e6069b23fb443558a2c7f7620b270971de4c1408b8"
+    else
+      odie "libyamlstar is not available for this Linux architecture"
+    end
+  end
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/yaml/yamlstar/releases/download/" \
+        "#{version}/libyamlstar-#{version}-macos-arm64.tar.xz"
+      sha256 "2e412306ad271071acaa89bef5c4edde59f95a3cd15a8a30558ec41efc7d2aa0"
+    else
+      odie "libyamlstar is not available for this macOS architecture"
+    end
+  end
+
+  def install
+    lib.install Dir["libyamlstar*.so*"]
+    lib.install Dir["libyamlstar*.dylib*"]
+    (include/"libyamlstar-#{version}").install Dir["*.h"]
+  end
+
+  test do
+    if OS.mac?
+      assert_predicate lib/"libyamlstar.dylib", :exist?
+    else
+      assert_predicate lib/"libyamlstar.so", :exist?
+    end
+  end
+end
